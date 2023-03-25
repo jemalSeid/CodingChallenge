1b1cb1e2193ee4334bd8e7a1fd50788d4125ee8c
Jemal Seid <125713385+jemalSeid@users.noreply.github.com>
2023-03-25 05:10:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -65,8 +65,84 @@ console.log(bmi1)
 console.log(bmi2)
 console.log(markHigherBMI)
 
-// out put of  bmi1 = 19.6
-// out put of  bmi2 = 17.5
+// if else exercise 
+// question #1
+
+if(bmi1 > bmi2) {
+    console.log("Mark's BMI is higher than John's")
+}else{
+    console.log("John's BMI is higher than Mark's BMI")
+}
+
+//question #2
+
+
+if(bmi1 > bmi2) {
+    console.log("Mark's BMI" , bmi1 , "is higher than John's BMI ", bmi2)
+}else{
+    console.log("John's BMI " , bmi2, "is higher than Mark's BMI ", bmi1)
+}
+
+/**Question 2.
+Part1**/
+const ageOfvoter = 15;
+
+if (ageOfvoter > 18) {
+   console.log("you are eligible to vote")
+}else {
+   console.log("you are not eligible to vote")
+}
+
+
+
+
+//part two
+
+const studentMark= 49
+
+
+if(studentMark >=93){
+    console.log("Outstanding")
+ }else if(studentMark < 93 && studentMark > 80) {
+    console.log("Excelent")
+ }else if(studentMark < 80 && studentMark >=75) {
+    console.log("Good job")
+ }else if (studentMark <75 && studentMark >= 50) {
+    console.log("Study hard to improve your grades")
+ }else{
+    console.log("You will need to study the course again to get better results")
+ }
+ 
+ //part three
+
+ const age = 8
+
+if(age > 0 && age <2) {
+   console.log("baby")
+}else if (age > 3 && age < 5) {
+   console.log("toddler")
+}else if(age >6 && age <12) {
+   console.log("Child")
+}else if(age >13 && age <18) {
+   console.log("teenager")
+}else{
+   console.log("adult")
+}
+
+
+
+//part four 
+
+const userNumber = 8
+
+if (userNumber == 0){
+   console.log("zero")
+}else if (userNumber > 0) {
+   console.log("positive number")
+}else {
+   console.log("negative number")
+}
+
 
 
 
